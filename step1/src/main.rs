118f894cb419bdae86167e9b9efb191d66fecd19
Bob Peters <contact@rust-trends.com>
2025-02-26 19:42:18 +0100

    Adding step1 of the dns-server

---
+++ step1/src/main.rs
@@ -0,0 +1,44 @@
+// src/main.rs
+use std::net::UdpSocket;
+
+mod dns;
+use dns::Header;
+
+// Debug print hex bytes of a buffer 16 bytes width followed by the ASCII representation of the bytes
+fn debug_print_bytes(buf: &[u8]) {
+    for (i, chunk) in buf.chunks(16).enumerate() {
+        print!("{:08x}: ", i * 16);
+        for byte in chunk {
+            print!("{:02x} ", byte);
+        }
+        for _ in 0..(16 - chunk.len()) {
+            print!("   ");
+        }
+        print!("  ");
+        for byte in chunk {
+            if *byte >= 32 && *byte <= 126 {
+                print!("{}", *byte as char);
+            } else {
+                print!(".");
+            }
+        }
+        println!();
+    }
+}
+
+fn main() {
+    let socket = UdpSocket::bind("0.0.0.0:1053").expect("Could not bind to port 1053");
+    let mut buf = [0; 512];
+    
+    println!("DNS server is running at port 1053");
+
+    loop {
+        let (len, addr) = socket.recv_from(&mut buf).expect("Could not receive data");
+        
+        println!("\nReceived query from {} of lenght {} bytes", addr, len);
+        debug_print_bytes(&buf[..len]);
+        
+        let header = Header::from_bytes(&buf[..len]).expect("Could not parse DNS header");
+        println!("\n{:?}", header);
+    }
+}
